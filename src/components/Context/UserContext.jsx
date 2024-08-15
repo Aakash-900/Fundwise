e326284fbc0b32dbc1023593bff2a8968f620cdb
Aakash-900 <aakashchdhry96@gmail.com>
2024-08-15 13:42:20 +0545

    update

--- src/components/Context/UserContext.jsx
+++ src/components/Context/UserContext.jsx
@@ -1,7 +1,7 @@
 
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
-import { toast } from 'react-toastify'; // Import toast
+import { toast } from 'react-toastify';
 
 const UserContext = createContext();
 
